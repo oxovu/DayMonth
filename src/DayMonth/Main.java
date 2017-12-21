a7e4a475110675a4cca740e5d35f16fbbc0ff477
oxovu <vlada318@live.com>
2017-12-21 15:08:04 +0300

    Readers modified

--- src/DayMonth/Main.java
+++ src/DayMonth/Main.java
@@ -4,8 +4,8 @@ package DayMonth;
 import java.util.Scanner;
 
 import static DayMonth.Game.*;
-import static DayMonth.Node.*;
 import static DayMonth.Readers.*;
+import static DayMonth.Node.*;
 
 public class Main {
 
@@ -13,21 +13,6 @@ public class Main {
     public static Pair lose = new Pair(31, 12); // проигрышная позиция
 
     public static void main(String[] arg) {
-     /*   Pair turn; // ход компьютера
-        Pair pair = new Pair(1, 1); // ход противника
-        while (!pair.equals(lose)) { // выполняется, пока кто-то не сходит в проигрышную позицию
-            turn = theGame(root).pair;
-            if (turn.equals(lose)) break;
-            System.out.println("Ваш ход");
-            pair = nextStep(turn);
-            root = pair;
-        }
-        scanner.close();
-        System.out.println("Конец игры");
-        if (pair.equals(lose)) {
-            System.out.println("Вы проиграли");
-        } else System.out.println("Вы выиграли");
-    } */
 
         Pair turn; // ход компьютера
         Pair pair = new Pair(1, 1); // ход противника
--- src/DayMonth/Readers.java
+++ src/DayMonth/Readers.java
@@ -1,5 +1,7 @@
 package DayMonth;
 
+import java.util.InputMismatchException;
+
 import static DayMonth.Main.*;
 import static DayMonth.Node.*;
 
@@ -8,47 +10,60 @@ public class Readers {
     // чтение даты для начала игры
 
     public static Pair start() {
-        System.out.println("Введите день: ");
-        int day = scanner.nextInt();
-        System.out.println("Введите месяц: ");
-        int month = scanner.nextInt();
-        Pair pair = new Pair(day, month);
-        while (!checkDate(day, month)) {
+        try {
+            System.out.println("Введите день: ");
+            int day = scanner.nextInt();
+            System.out.println("Введите месяц: ");
+            int month = scanner.nextInt();
+            Pair pair = new Pair(day, month);
+            while (!checkDate(day, month)) {
+                System.out.println("Некорректная дата. Введите новую");
+                pair = start();
+                break;
+            }
+            return pair;
+        } catch (Exception e) {
             System.out.println("Некорректная дата. Введите новую");
-            pair = start();
-            break;
+            scanner.nextLine();
+            root = start();
         }
-        return pair;
+        return null;
     }
 
     // промежуточное чтение даты
 
     public static Pair nextStep(Pair turn) {
-        System.out.println("Введите день для продолжения игры: ");
-        int day = scanner.nextInt();
-        System.out.println("Введите месяц: ");
-        int month = scanner.nextInt();
-        Pair pair = new Pair(day, month);
-        while (!checkDate(day, month)) {
+        try {
+            System.out.println("Введите день для продолжения игры: ");
+            int day = scanner.nextInt();
+            System.out.println("Введите месяц: ");
+            int month = scanner.nextInt();
+            Pair pair = new Pair(day, month);
+            while (!checkDate(day, month)) {
+                System.out.println("Некорректная дата. Введите новую");
+                pair = nextStep(turn);
+                break;
+            }
+            while (!checkTurnDate(day, month, turn)) {
+                System.out.println("Невозможный ход. Введите новую дату");
+                pair = nextStep(turn);
+                break;
+            }
+            return pair;
+        } catch (Exception e) {
             System.out.println("Некорректная дата. Введите новую");
-            pair = nextStep(turn);
-            break;
+            scanner.nextLine();
+            return nextStep(turn);
         }
-        while (!checkTurnDate(day, month, turn)){
-            System.out.println("Невозможный ход. Введите новую дату");
-            pair = nextStep(turn);
-            break;
-        }
-        return pair;
     }
 
     // проверка возможности хода игрока
 
     private static boolean checkTurnDate(int day, int month, Pair lastTurn) {
-        if (day == lastTurn.day + 1 && month == lastTurn.month) return  true;
-        if (day == lastTurn.day + 2 && month == lastTurn.month) return  true;
-        if (day == lastTurn.day && month == lastTurn.month + 1) return  true;
-        if (day == lastTurn.day && month == lastTurn.month + 1) return  true;
+        if (day == lastTurn.day + 1 && month == lastTurn.month) return true;
+        if (day == lastTurn.day + 2 && month == lastTurn.month) return true;
+        if (day == lastTurn.day && month == lastTurn.month + 1) return true;
+        if (day == lastTurn.day && month == lastTurn.month + 2) return true;
         return false;
     }
 }
